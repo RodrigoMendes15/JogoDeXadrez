1996dedc2a50cdcc31fae4a4223023c27bb7b725
Rodrigo Mendes <rodrigo.mendes15@hotmail.com>
2019-09-05 15:15:23 -0300

    Testes iniciais com movimentos possiveis das peças

--- Program.cs
+++ Program.cs
@@ -17,15 +17,21 @@ namespace SistemaXadrez
                     Console.Clear();
                     Tela.imprimirTabuleiro(partida.tab);
 
+                    Console.WriteLine();
                     Console.Write("Origem: ");
                     Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+
+                    bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
+
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
+
+                    Console.WriteLine();
                     Console.Write("Destino: ");
                     Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
 
                     partida.executaMovimento(origem, destino);
                 }
-
-                
             }
             catch(TabuleiroException e)
             {
