5c134a30609aafcf57c9cff6fb73b8c2170b42be
Rodrigo Mendes <rodrigo.mendes15@hotmail.com>
2019-09-06 16:50:27 -0300

    Jogadas Roque Pequeno e Grande inseridas

--- Xadrez/Peao.cs
+++ Xadrez/Peao.cs
@@ -9,18 +9,18 @@ namespace xadrez
         }
         public override string ToString()
         {
-            return "p";
+            return "P";
         }
 
         private bool existeInimigo(Posicao pos)
         {
             Peca p = tab.peca(pos);
-            return p != null && p.cor != this.cor;
+            return p != null && p.cor != cor;
         }
 
         private bool livre(Posicao pos)
         {
-            return tab.peca(pos) != null;
+            return tab.peca(pos) == null;
         }
 
         public override bool[,] movimentosPossiveis()
